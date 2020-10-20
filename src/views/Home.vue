8004bd611ad9eb517a2064b1f588d9bf8f2f68bc
Jonathan Particular <jonathan.afranio.s@gmail.com>
2020-10-20 01:19:19 -0300

    adicionar slick no vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,96 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <section class="hero home is-dark is-fullheight">
+    <div class="hero-body">
+      <div class="container">
+        <!--<VueSlickCarousel :arrows="true" :dots="false" :responsive="settings.responsive">-->
+        <VueSlickCarousel v-bind="settings">
+          <router-link v-for="(serie, index) in allSeries" v-bind:key="index" :to="`/serie/${serie.id}`" :title="serie.name">
+            <img :src="serie.image" :alt="serie.name">
+          </router-link>
+          
+        </VueSlickCarousel>
+        <!--<img alt="Vue logo" src="../assets/logo.png">-->
+        <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import VueSlickCarousel from "vue-slick-carousel";
+import "vue-slick-carousel/dist/vue-slick-carousel.css";
+// optional style for arrows & dots
+import "vue-slick-carousel/dist/vue-slick-carousel-theme.css";
+//import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    VueSlickCarousel
+    //HelloWorld
+  },
+  data(){
+    return {
+      series: [],
+      settings: {
+        slidesToShow: 5,
+        slideToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 1280,
+            settings: {
+              slidesToShow: 4
+            }
+          },
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3
+            }
+          },
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1
+            }
+          }
+        ]
+      }
+    }
+  },
+  created(){
+    fetch('http://psdtohtmlandcss.com.br/stream-vue/wordpress/wp-json/wp/v2/series')
+    .then(res => res.json())
+    .then(res => {
+      let carouselArray = res.map((item) => {
+        let carouselItem = {
+          image: item.acf.imagem,
+          name: item.name,
+          id: item.id
+        }
+        return carouselItem
+      });
+      this.series = carouselArray;
+      //console.log('carouselArray', carouselArray);
+    })
+  },
+  computed: {
+      allSeries(){
+          return this.series;
+      }
+  },
+};
 </script>
+
+
+<style lang="sass">
+.slick-slide
+  border: 1px solid red
+</style>
