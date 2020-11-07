7a3766348e506cfde95b1c3c9b992a676f75611b
Jonathan Particular <jonathan.afranio.s@gmail.com>
2020-11-07 03:49:41 -0300

    remover componente about

--- src/router/index.js
+++ src/router/index.js
@@ -28,11 +28,8 @@ const routes = [
         component: () => import(/* webpackChunkName: "epsodio" */ '../views/Epsodios.vue')
     },
     {
-        path: '/about',
-        name: 'About',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+        path: '*',
+        redirect: '/'
     }
 ]
 
