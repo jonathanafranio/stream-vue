39a58901b8de708b5a70edf3c2f7ff21e7a71a1f
Jonathan Particular <jonathan.afranio.s@gmail.com>
2021-01-03 23:34:02 -0300

    d591d5c

---
+++ precache-manifest.6edae37ee6df7c09bf33f8b5502070bb.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f37750b3a415b78441a7",
+    "url": "/css/app.410e8132.css"
+  },
+  {
+    "revision": "ad5774200426ac75061d",
+    "url": "/css/chunk-vendors.85471b0f.css"
+  },
+  {
+    "revision": "823557777bb73296e9d1",
+    "url": "/css/epsodio.0949c108.css"
+  },
+  {
+    "revision": "f5fbf1c4405cd9f92f2f",
+    "url": "/css/serie.bcfdc5f6.css"
+  },
+  {
+    "revision": "d149302a8565c4ec77a7",
+    "url": "/css/serie~temporada.4e61c945.css"
+  },
+  {
+    "revision": "f480d62f84c68846ad8b",
+    "url": "/css/temporada.bcfdc5f6.css"
+  },
+  {
+    "revision": "46ac6629168546c71dbf115992aa4be9",
+    "url": "/fonts/VideoJS.46ac6629.eot"
+  },
+  {
+    "revision": "8300bd7f30e0a313c1d772b49d96cb8e",
+    "url": "/fonts/fa-brands-400.8300bd7f.ttf"
+  },
+  {
+    "revision": "ad527cc5ec23d6da66e8a1d6772ea6d3",
+    "url": "/fonts/fa-brands-400.ad527cc5.woff"
+  },
+  {
+    "revision": "e2ca6541bff3a3e9f4799ee327b28c58",
+    "url": "/fonts/fa-brands-400.e2ca6541.eot"
+  },
+  {
+    "revision": "f075c50f89795e4cdb4d45b51f1a6800",
+    "url": "/fonts/fa-brands-400.f075c50f.woff2"
+  },
+  {
+    "revision": "3c6879c4f342203d099bdd66dce6d396",
+    "url": "/fonts/fa-regular-400.3c6879c4.woff"
+  },
+  {
+    "revision": "49f00693b0e5d45097832ef5ea1bc541",
+    "url": "/fonts/fa-regular-400.49f00693.ttf"
+  },
+  {
+    "revision": "4a74738e7728e93c4394b8604081da62",
+    "url": "/fonts/fa-regular-400.4a74738e.woff2"
+  },
+  {
+    "revision": "b01516c1808be557667befec76cd6318",
+    "url": "/fonts/fa-regular-400.b01516c1.eot"
+  },
+  {
+    "revision": "205f07b3883c484f27f40d21a92950d4",
+    "url": "/fonts/fa-solid-900.205f07b3.ttf"
+  },
+  {
+    "revision": "4451e1d86df7491dd874f2c41eee1053",
+    "url": "/fonts/fa-solid-900.4451e1d8.woff"
+  },
+  {
+    "revision": "8ac3167427b1d5d2967646bd8f7a0587",
+    "url": "/fonts/fa-solid-900.8ac31674.eot"
+  },
+  {
+    "revision": "8e1ed89b6ccb8ce41faf5cb672677105",
+    "url": "/fonts/fa-solid-900.8e1ed89b.woff2"
+  },
+  {
+    "revision": "2f12242375edd68e9013ecfb59c672e9",
+    "url": "/img/fa-brands-400.2f122423.svg"
+  },
+  {
+    "revision": "3602b7e8b2cb1462b0bef9738757ef8a",
+    "url": "/img/fa-regular-400.3602b7e8.svg"
+  },
+  {
+    "revision": "664de3932dd6291b4b8a8c0ddbcb4c61",
+    "url": "/img/fa-solid-900.664de393.svg"
+  },
+  {
+    "revision": "9836eb7ed715fe023329288631827b88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f37750b3a415b78441a7",
+    "url": "/js/app.ee43da99.js"
+  },
+  {
+    "revision": "ad5774200426ac75061d",
+    "url": "/js/chunk-vendors.cd3b0ce8.js"
+  },
+  {
+    "revision": "823557777bb73296e9d1",
+    "url": "/js/epsodio.e8ceaaa0.js"
+  },
+  {
+    "revision": "f5fbf1c4405cd9f92f2f",
+    "url": "/js/serie.c546677c.js"
+  },
+  {
+    "revision": "d149302a8565c4ec77a7",
+    "url": "/js/serie~temporada.1341828b.js"
+  },
+  {
+    "revision": "f480d62f84c68846ad8b",
+    "url": "/js/temporada.a209e3f2.js"
+  },
+  {
+    "revision": "69dd71b6e56f67196cf28542acbf454b",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
